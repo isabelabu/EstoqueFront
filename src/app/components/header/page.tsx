5d2d7f71855f4d91e55001232f414f40ec3e9583
Isabela Bueno <isa.belabu@hotmail.com>
2024-05-16 22:15:56 -0300

    botao cadastrar produto

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -1,9 +1,15 @@
+'use client';
 import styles from "./page.module.css";
 
 function Header(){
+    const cadastrarProduto = () => {
+      alert("Cadastrar novo produto");
+    };
+
     return(
       <div className={styles.header}>
         <h1>PRODUTOS NO ESTOQUE</h1>
+        <button className={styles.cadastro} onClick={cadastrarProduto}>Cadastrar produto</button>
       </div>
     );
 }
