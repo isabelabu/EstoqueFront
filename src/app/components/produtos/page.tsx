fbeb8f2cc30a6f5ca69876cf9a6c05382d69df82
Isabela Bueno <isa.belabu@hotmail.com>
2024-05-17 10:51:33 -0300

    estilizacao da pagina de estoque

--- src/app/components/produtos/page.tsx
+++ src/app/components/produtos/page.tsx
@@ -13,10 +13,22 @@ function CardProduto(){
     return(
         <div className={styles.main}>
             <div className={styles.info}>
-                <h3>Nome do produto</h3>
-                <p>Descrição do produto</p>
-                <p>Valor do produto</p>
-                <Image src="/camiseta.jpg" alt="Teste" width={200} height={200}></Image>
+                <tr>
+                    <th>ID</th>
+                    <th>Imagem</th>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Valor</th>
+                    <th>Quantidade</th>
+                </tr>
+                <tr>
+                    <td>#</td>
+                    <td><Image src="/camiseta.jpg" alt="Teste" width={100} height={100}></Image></td>
+                    <td>Nome do produto</td>
+                    <td>Descrição do produto</td>
+                    <td>R$ 0,00</td>
+                    <td>1</td>
+                </tr>
             </div>
           <div className={styles.acoes}>
             <button className={styles.excluir} onClick={excluirProduto}>Excluir</button>
